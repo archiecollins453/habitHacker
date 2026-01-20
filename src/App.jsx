de35e74f237c10ffec67c3fc26db32dacea2f012
archiecollins453 <budderjack14@gmail.com>
2026-01-20 14:14:41 +0200

    Login and Dashboard setup

--- src/App.jsx
+++ src/App.jsx
@@ -2,33 +2,25 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Login from "./pages/Login";
+import Dashboard from "./pages/Dashboard";
+import HabitDetails from "./pages/HabitDetails";
+import { useAuth } from "./hooks/useAuth"; // optional
+import { BrowserRouter, Routes } from 'react-router-dom'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
+  const {user} = useAuth();
 
+  console.log(user)
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path = "/login" element={<Login />}/>
+        <Route path="/" element={user ? <Dashboard /> : <Login />} />
+        <Route path="/habit/:id" element={user ? <HabitDetails /> : <Login />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
